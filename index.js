0891022904aad2049f686391a92065c7c1f3b79f
Jesus <6.jesus.a@gmail.com>
2024-03-26 20:25:36 -0300

    Termine el ejercicio 3.6, agregando los status de error del post

--- index.js
+++ index.js
@@ -1,83 +1,94 @@
 const express = require(`express`);
 const app = express();
 
-app.use((request,response,next)=>{
+app.use((request, response, next) => {
   request.timestamp = new Date().toLocaleString();
   next();
 });
 app.use(express.json());
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
-app.post(`/api/persons`,(request,response)=>{
-  const {name,number} = request.body;
-  const id = Math.floor(Math.random()*1000);
-  const contenido = {
-    name,
-    number,
-    id
-  }
-  console.log(contenido);
-})
+app.post(`/api/persons`, (request, response) => {
+  const { name, number } = request.body;
+  const namesPersons = persons.some((info) => info.name === name);
 
-app.delete(`/api/persons/:id`,(request,response)=>{
+  if (!name || !number) {
+    console.log("falta informacion");
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  if (namesPersons) {
+    return response.status(400).json({ error: "name must be unique" });
+  } else {
+    const id = Math.floor(Math.random() * 1000);
+    const newData = {
+      id,
+      name,
+      number,
+    };
+    persons = persons.concat(newData);
+    console.log(newData);
+    response.json(newData);
+  }
+});
+
+app.delete(`/api/persons/:id`, (request, response) => {
   const id = parseInt(request.params.id);
-  persons = persons.filter(info => info.id !== id);
+  persons = persons.filter((info) => info.id !== id);
   response.status(204).end();
   console.log(persons);
+});
 
-})
-
-app.get(`/api/persons/:id`,(request,response)=>{
+app.get(`/api/persons/:id`, (request, response) => {
   const id = parseInt(request.params.id);
-  const data = persons.find(info => info.id === id);
-  if (data){
+  const data = persons.find((info) => info.id === id);
+  if (data) {
     response.json(data);
-  }
-  else{
-    response.statusMessage="The id was not found"
+  } else {
+    response.statusMessage = "The id was not found";
     response.status(404).end();
-    
   }
+});
 
-})
-
-app.get(`/api/persons`,(request,response)=>{
+app.get(`/api/persons`, (request, response) => {
   response.json(persons);
-})
+});
 
-app.get(`/info`,(request,response)=>{
+app.get(`/info`, (request, response) => {
   const numero = persons.length;
-  response.send(`<h1>Esta agenda tiene ${numero} contactos...  La hora ${request.timestamp}</h1>`);
-})
+  response.send(
+    `<h1>Esta agenda tiene ${numero} contactos...  La hora ${request.timestamp}</h1>`
+  );
+});
 
-app.get(`/`,(request,response)=>{
-    response.send(`<h1>Servidor en funcionamiento</h1>`);
-    console.log("Alguna cosa funciona");
-})
+app.get(`/`, (request, response) => {
+  response.send(`<h1>Servidor en funcionamiento</h1>`);
+  console.log("Alguna cosa funciona");
+});
 
 const PORT = 3001;
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
