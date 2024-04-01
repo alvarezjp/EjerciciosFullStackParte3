9a2c97b52f794614dcb3ae5512543d46c0c27436
Jesus <6.jesus.a@gmail.com>
2024-04-01 20:42:33 -0300

    agregar cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require(`express`);
 const morgan = require(`morgan`);
+const cors = require(`cors`);
 const app = express();
 
+app.use(cors());
 app.use((request, response, next) => {
   request.timestamp = new Date().toLocaleString();
   next();
