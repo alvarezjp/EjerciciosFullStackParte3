2dd07a366e1b56e130f516f0b32a70cdaa25f9d7
Jesus <6.jesus.a@gmail.com>
2024-04-02 19:39:04 -0300

    agregar archivo dist y agergar middleware static

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require(`cors`);
 const app = express();
 
 app.use(cors());
+app.use(express.static(`dist`))
 app.use((request, response, next) => {
   request.timestamp = new Date().toLocaleString();
   next();
